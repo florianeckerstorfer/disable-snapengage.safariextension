02ab75dd60b7b334657f60531e37ab09903f331c
Florian Eckerstorfer <f.eckerstorfer@gmail.com>
2011-10-11 10:32:31 +0200

    Fixed bug.

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-var scripts = document.getElementsByTagName('script');
-for (var i = 0; i < scripts.length; ++i)
+function disableSnapEngage()
 {
-	if (scripts[i].src.indexOf('snapengage') != -1)
+	var scripts = document.getElementsByTagName('script');
+	for (var i = 0; i < scripts.length; ++i)
 	{
-		scripts[i].outerHTML = '';
+		if (scripts[i].src.indexOf('snapengage') != -1 || scripts[i].src.indexOf('snapabug') != -1 || scripts[i].innerHTML.indexOf('snapengage') != -1)
+		{
+			scripts[i].parentNode.removeChild(scripts[i]);
+			// scripts[i].outerHTML = '';
+		}
+	}
+	var div1 = document.getElementById('SnapABug_W');
+	div1.parentNode.removeChild(div1);
+	var div2 = document.getElementById('SnapABug_WP');
+	div2.parentNode.removeChild(div2);
+	var div3 = document.getElementById('SnapABug_Applet');
+	div3.parentNode.removeChild(div3);
+	var div4 = document.getElementsByClassName('SnapABug_Button');
+	for (var i = 0; i < div4.length; ++i)
+	{
+		div4[i].parentNode.removeChild(div4[i]);
 	}
 }
+window.setTimeout('disableSnapEngage();', 5000);
